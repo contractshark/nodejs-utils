7e3e642f8831931ae24dd8698fce10605d608ba3
sam <sam@freighttrust.com>
2021-01-25 07:23:01 -0800

    feat(0.1.0): inital commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const log_1 = require("./src/log");
+const option_1 = require("./src/option");
+exports.default = {
+  Log: log_1.default,
+  Option: option_1.default,
+};
